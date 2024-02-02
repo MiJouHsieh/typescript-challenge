6fda8de2b4ddd3d59d707ed9af8ed852459cda10
MiJouHsieh <mijouhsieh@gmail.com>
2024-02-03 02:58:05 +0800

    initialValue with comma not plus

--- src/asyncSum.ts
+++ src/asyncSum.ts
@@ -8,8 +8,10 @@
 
 export function asyncSum(numbers: number[]): Promise<number> {
     return new Promise((resolve, reject) => {
+        const initialValue = 0;
         try {
             // 請在此處寫下你的程式碼
+            resolve(numbers.reduce((acc, cur) => acc + cur, initialValue))
         } catch (error) {
             reject(error);
         }
